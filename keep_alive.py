6689ad5e3d30d1d4250e638aed609b2bf0866eb1
testert353 <secondrobloxplayer@gmail.com>
2025-04-19 01:10:41 -0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive"
+    return "This script is running! Be sure to start Testert's Reddit Karma Farmer!"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
